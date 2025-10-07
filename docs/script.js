11e6cbfcaf7861fdc224b4da84f47f530f956bbc
spendingmysummer <schlccntng@gmail.com>
2025-10-06 23:29:52 -0700

    updated script.js

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
 const tombolSapa = document.getElementById('sapabutton');
-  tombolSapa.addEventListener('click', function() {
-    alert('Hi');
-  });
+tombolSapa.addEventListener('click', function() {
+  alert('Hi, Nino Refayandy! Glad to meet you!');
+});
